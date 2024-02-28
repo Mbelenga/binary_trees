d078f19e558f69a818b6004645fec205ed06ad42
Mbelenga <mbelengaterence@gmail.com>
2024-02-29 01:44:44 +0300

    header file

---
+++ binary_trees.h
@@ -0,0 +1,11 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
