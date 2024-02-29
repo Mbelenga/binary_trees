28f3dd5dba8f0d166f7b02eabd32bd3aca6420a7
Mbelenga <mbelengaterence@gmail.com>
2024-02-29 04:47:49 -0800

    Header

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- *
+ *binary_tree_preorder - A function that goes through a binary tree using preorder traversal
  *
  *
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)
