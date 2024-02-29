240bbb0727bdde88cecf129d6ebae435da9d6640
Mbelenga <mbelengaterence@gmail.com>
2024-02-29 04:35:07 -0800

    error

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,14 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
- *
+ *binary_tree_is_root - checks if the given node is a root
+ *@node: a pointer to the node
+ *Return: return 1 if node is a root and 0 if otherwise
  */
 int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+	return (0);
