e28aafd471eef8455cfaf18b1747412b9187821d
Mbelenga <mbelengaterence@gmail.com>
2024-03-01 04:28:07 -0800

    corrections

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,6 +9,10 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		RETURN (0);
+		return (0);
 
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right))
+	size = binary_tree_size(tree->left);
+	size = binary_tree_size(tree->right);
+
+	return (size + size + 1);
+}
