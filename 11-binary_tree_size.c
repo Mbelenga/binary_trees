aca381aa29114ab967662fa883e2897a17b6bae7
Mbelenga <mbelengaterence@gmail.com>
2024-03-01 04:17:47 -0800

    corrections

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,4 +6,9 @@
  * If tree is NULL, the function must return 0
  */
 
-size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		RETURN (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right))
