a3f2c69d4e106f9addd87f49c2c33fc19cefd514
Mbelenga <mbelengaterence@gmail.com>
2024-03-01 13:57:10 -0800

    A function thta checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -28,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t leftheight = 0, rightheight = 0;
 
 	if (tree == NULL)
 		return (0);
