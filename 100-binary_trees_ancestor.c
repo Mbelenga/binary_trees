10d3117f1fb0ad9bfac092e19fb7ea5498155df8
Mbelenga <mbelengaterence@gmail.com>
2024-03-02 13:02:39 -0800

    a function that finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,20 +1,42 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - finds the lowest common ancestor
- * @first: pointer to the first node
- * @second: pointer to the second node
- * Return: pointer to the LCA || NULL on failure
- */
-
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: a pointer to the first node
+ * @second: a pointer to the second node
+ *
+ * Return: a pointer to the lowest common ancestor node of the two given nodes.
+ * Otherwise NULL
+*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
+const binary_tree_t *second)
 {
-	const binary_tree_t *leftLCA, *rightLCA;
+	const binary_tree_t *ancestors_set[1024];
+	size_t first_ancestors_count = 0, i;
 
-	for (leftLCA = first; leftLCA; leftLCA = leftLCA->parent)
-		for (rightLCA = second; rightLCA; rightLCA = rightLCA->parent)
-			if (leftLCA == rightLCA)
-				return ((binary_tree_t *)leftLCA);
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	/* Traverse the ancestors of the first node and add them to the set */
+	while (first != NULL)
+	{
+		ancestors_set[first_ancestors_count++] = first;
+		first = first->parent;
+	}
+
+	/* Traverse the ancestors of the second node and check for common ancestor */
+	while (second != NULL)
+	{
+		for (i = 0; i < first_ancestors_count; i++)
+		{
+			if (second == ancestors_set[i])
+				return ((binary_tree_t *)second);
+		}
+		second = second->parent;
+
+	}
+
+	/* No common ancestors */
 	return (NULL);
+
 }
